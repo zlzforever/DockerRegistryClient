78d0e2aa68dc102db26bbee575140b36c9b0897d
Lewis <lewis@LewisdeMac-Pro.local>
2019-08-01 17:55:16 +0800

    fix bugs

---
+++ src/DockerRegistryClient.Cli/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace DockerRegistryClient.Cli
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                PrintUsage();
+                return;
+            }
+
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddCommandLine(args, new Dictionary<string, string>
+            {
+                {"-X", "ACT"},
+                {"-R", "REPO"},
+                {"-U", "USER"},
+                {"-P", "PWD"}
+            });
+            var configuration = configurationBuilder.Build();
+            var action = configuration["ACT"];
+            var defaultColor = Console.ForegroundColor;
+            switch (action)
+            {
+                case "DELETE":
+                {
+                    var image = configuration["REPO"];
+                    if (string.IsNullOrWhiteSpace(image))
+                    {
+                        PrintUsage();
+                        break;
+                    }
+
+                    var uri = new Uri(image);
+                    var registry = $"{uri.Scheme}://{uri.Authority}";
+                    var imageWithTag = uri.AbsolutePath.Substring(1, uri.AbsolutePath.Length - 1);
+                    var lastColon = imageWithTag.LastIndexOf(":", StringComparison.Ordinal);
+                    var imageName = imageWithTag.Substring(0, lastColon);
+                    var tag = imageWithTag.Substring(lastColon + 1, imageWithTag.Length - lastColon - 1);
+                    var result = await DockerRegistryClient.DeleteImage(new Uri(registry), imageName, tag,
+                        configuration["USER"],
+                        configuration["PWD"]);
+                    if (result)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine($"Delete image {imageName}:{tag} success");
+                        Console.WriteLine("");
+                    }
+                    else
+                    {
+                        Console.WriteLine("");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"Delete image {imageName}:{tag} failed");
+                        Console.ForegroundColor = defaultColor;
+                        Console.WriteLine("");
+                    }
+
+                    break;
+                }
+
+                case "REPO":
+                {
+                    var registry = configuration["REPO"];
+                    if (string.IsNullOrWhiteSpace(registry))
+                    {
+                        PrintUsage();
+                        break;
+                    }
+                    var uri = new Uri(registry);
+                    var repositories =
+                        await DockerRegistryClient.GetRepositories(uri, configuration["USER"], configuration["PWD"]);
+                    if (repositories != null)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine($"Repositories: {JsonConvert.SerializeObject(repositories)}");
+                        Console.WriteLine("");
+                    }
+                    else
+                    {
+                        Console.WriteLine("");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Fetch repositories failed");
+                        Console.ForegroundColor = defaultColor;
+                        Console.WriteLine("");
+                    }
+
+                    break;
+                }
+
+                case "TAG":
+                {
+                    var image = configuration["REPO"];
+                    if (string.IsNullOrWhiteSpace(image))
+                    {
+                        PrintUsage();
+                        break;
+                    }
+                    var uri = new Uri(image);
+                    var registry = $"{uri.Scheme}://{uri.Authority}";
+                    var imageName = uri.AbsolutePath.Substring(1, uri.AbsolutePath.Length - 1);
+                    var tags =
+                        await DockerRegistryClient.GetTags(new Uri(registry), imageName, configuration["USER"],
+                            configuration["PWD"]);
+                    if (tags != null)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine($"Image: {imageName}, Tags: {JsonConvert.SerializeObject(tags)}");
+                        Console.WriteLine("");
+                    }
+                    else
+                    {
+                        Console.WriteLine("");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"Fetch image {imageName} tag failed");
+                        Console.ForegroundColor = defaultColor;
+                        Console.WriteLine("");
+                    }
+
+                    break;
+                }
+
+                default:
+                {
+                    PrintUsage();
+                    break;
+                }
+            }
+        }
+
+        static void PrintUsage()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Usage: drc [OPTIONS]");
+            Console.WriteLine("");
+            Console.WriteLine("Options:");
+            Console.WriteLine("  -X       DELETE, REPO, TAG");
+            Console.WriteLine("  -R       Repository or registry address");
+            Console.WriteLine("  -U       User");
+            Console.WriteLine("  -P       Password");
+            Console.WriteLine("");
+            Console.WriteLine("Examples:");
+            Console.WriteLine("  Delete image:    drc -X DELETE -R http://localhost:5000/test:latest");
+            Console.WriteLine("  List repository: drc -X REPO   -R http://localhost:5000");
+            Console.WriteLine("  List tag:        drc -X TAG    -R http://localhost:5000/test");
+        }
+    }
+}
---
+++ src/DockerRegistryClient/DockerRegistryClient.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DockerRegistryClient
+{
+    public static class DockerRegistryClient
+    {
+        private static readonly Dictionary<string, HttpClient> HttpClientDict = new Dictionary<string, HttpClient>();
+
+        public static async Task<List<string>> GetRepositories(Uri registry, string user = null,
+            string password = null)
+        {
+            var client = GetHttpClient(registry);
+
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, $"{registry}v2/_catalog");
+            ConfigureAuthorization(httpRequestMessage, user, password);
+            var response = await client.SendAsync(httpRequestMessage);
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                var jsonObject = JsonConvert.DeserializeObject<JObject>(json);
+                var repositories = jsonObject.SelectTokens("$.repositories[*]").Select(x => x.ToString()).ToList();
+                return repositories;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static async Task<List<string>> GetTags(Uri registry, string image, string user = null,
+            string password = null)
+        {
+            var client = GetHttpClient(registry);
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, $"{registry}v2/{image}/tags/list");
+            ConfigureAuthorization(httpRequestMessage, user, password);
+            var response = await client.SendAsync(httpRequestMessage);
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                var jsonObject = JsonConvert.DeserializeObject<JObject>(json);
+                var tags = jsonObject.SelectTokens("$.tags[*]").Select(x => x.ToString()).ToList();
+                return tags;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static async Task<bool> DeleteImage(Uri registry, string image, string tag, string user = null,
+            string password = null)
+        {
+            var client = GetHttpClient(registry);
+            var requestSha256Message = new HttpRequestMessage(HttpMethod.Get,
+                $"{registry}v2/{image}/manifests/{tag}");
+            ConfigureAuthorization(requestSha256Message, user, password);
+            requestSha256Message.Headers.Accept.TryParseAdd(
+                "application/vnd.docker.distribution.manifest.v2+json");
+            var response = await client.SendAsync(requestSha256Message);
+            if (response.IsSuccessStatusCode)
+            {
+                var sha256 = response.Headers.ETag.Tag.Replace("\"", "");
+                var deleteMessage =
+                    new HttpRequestMessage(HttpMethod.Delete, $"{registry}v2/{image}/manifests/{sha256}");
+                ConfigureAuthorization(deleteMessage, user, password);
+
+                response = await client.SendAsync(deleteMessage);
+
+                return response.IsSuccessStatusCode;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static HttpClient GetHttpClient(Uri registry)
+        {
+            var key = registry.ToString();
+            if (!HttpClientDict.ContainsKey(key))
+            {
+                HttpClientDict.Add(key, new HttpClient());
+            }
+
+            return HttpClientDict[key];
+        }
+
+        private static void ConfigureAuthorization(HttpRequestMessage httpRequestMessage, string user, string password)
+        {
+            string token = null;
+            if (!string.IsNullOrWhiteSpace(user))
+            {
+                token = Convert.ToBase64String(
+                    Encoding.UTF8.GetBytes($"{user}:{password}"));
+            }
+
+            if (!string.IsNullOrWhiteSpace(token))
+            {
+                httpRequestMessage.Headers.TryAddWithoutValidation("Authorization", $"Basic {token}");
+            }
+        }
+    }
+}
